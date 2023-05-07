37907b98872461454c5c25ce581ca1f3b060e472
Ilya Baranovskiy <baranovskiydev@gmail.com>
2023-05-07 18:56:23 +0300

    (Client development)встретились как-то x y и z

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Header from './header/Header';
 import styles from './app.module.css'
-import RegForm from './regform/RegForm';
+import RegForm from './Form/RegForm';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Mypage from '../pages/mypage/Mypage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className={styles.layout}>
         <Header/>
         <Routes>
+          <Route path='/' element={<Mypage/>}/>
           <Route path='registration' element = {<RegForm/>}/>
         </Routes>
       </div>
