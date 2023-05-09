6ec9c0b90df58084c00b851f18da87ba1bda6fae
Ilya Baranovskiy <baranovskiydev@gmail.com>
2023-05-10 00:42:14 +0300

    i wanna cry

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
 import React, {useEffect} from 'react'
-import { Link } from "react-router-dom";
+import { Link, generatePath, useParams } from "react-router-dom";
 import styles from "./header.module.css"
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { userSlice } from '../../store/reducers/userSlice';
@@ -9,8 +9,10 @@ import { render } from '@testing-library/react';
 
 export default function Header(): ReactJSXElement {
 
-  const {user, isAuth} = useAppSelector(state => state.userReducer);
+  const {user, isAuth, isLoading} = useAppSelector(state => state.userReducer);
   const dispatch = useAppDispatch();
+
+
 
   return (
 
@@ -22,11 +24,11 @@ export default function Header(): ReactJSXElement {
         <div className={styles.navbar__links}>
 
         {/* No Auth */}
-        {!isAuth && <Link to='registration'><div className={styles.btn}>Регистрация</div></Link>}
-        {!isAuth && <Link to='login'><div className={styles.btn}>Логин</div></Link>}
+        {!isAuth && !isLoading && <Link to='registration'><div className={styles.btn}>Регистрация</div></Link>}
+        {!isAuth && !isLoading && <Link to='login'><div className={styles.btn}>Логин</div></Link>}
 
         {/* Authed */}
-        {isAuth && <div className={styles.avatar} ></div>}
+        {isAuth && <Link to=':id'><div className={styles.avatar} ></div></Link>}
         {isAuth && <div className={styles.btn} onClick={() => dispatch(userSlice.actions.logoutUser())}>Выход </div>}
         </div>
     </header>
