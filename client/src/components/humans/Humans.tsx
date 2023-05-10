64d2c29ae5630b4bc7c553c664813c7861157d63
Ilya Baranovskiy <baranovskiydev@gmail.com>
2023-05-10 22:38:26 +0300

    client working hard, wanna die

---
+++ client/src/components/humans/Humans.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './humans.module.css'
+
+export default function Humans() {
+  return (
+    <div className={styles.page}>
+        <h1>Люди карочи</h1>
+    </div>
+  )
+}
